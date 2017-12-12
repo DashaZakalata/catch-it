d32ff57406aecd3d3f06360177517f4226155c0a
Dasha <zakalata1998@mail.ru>
2017-12-12 22:54:41 +0200

    Создали объекты игры, их методы передвижения

--- app/app/Form1.Designer.cs
+++ app/app/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.Icon1 = new System.Windows.Forms.PictureBox();
+            this.Icon2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Icon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Icon2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = global::app.Properties.Resources.prog;
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Location = new System.Drawing.Point(-7, 252);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(173, 163);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Icon1
+            // 
+            this.Icon1.Location = new System.Drawing.Point(130, 29);
+            this.Icon1.Name = "Icon1";
+            this.Icon1.Size = new System.Drawing.Size(65, 50);
+            this.Icon1.TabIndex = 1;
+            this.Icon1.TabStop = false;
+            // 
+            // Icon2
+            // 
+            this.Icon2.Location = new System.Drawing.Point(285, 29);
+            this.Icon2.Name = "Icon2";
+            this.Icon2.Size = new System.Drawing.Size(65, 50);
+            this.Icon2.TabIndex = 2;
+            this.Icon2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::app.Properties.Resources.xp_windows_vektor_74137_2560x1440;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(634, 411);
+            this.Controls.Add(this.Icon2);
+            this.Controls.Add(this.Icon1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Icon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Icon2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox Icon1;
+        private System.Windows.Forms.PictureBox Icon2;
     }
 }
 
--- app/app/Form1.cs
+++ app/app/Form1.cs
@@ -9,12 +9,94 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace app
-{
-    public partial class Form1 : Form
     {
-        public Form1()
+        public partial class Form1 : Form
         {
-            InitializeComponent();
+            private readonly Timer updateTimer;
+
+            public Form1()
+            {
+                updateTimer = new Timer();
+                updateTimer.Interval = 1000; //переодичность, частота 150мсек
+                updateTimer.Tick += UpdateTimer_Tick;
+                updateTimer.Enabled = true;
+                InitializeComponent();
+                DoubleBuffered = true; //убирает мелькание
+            }
+
+            public void dropIcon(Point[] start, PictureBox[] Icon)
+            {
+                Random tic = new Random();
+                int rand = tic.Next(0, 1);
+
+                int[] checkArr = new int[1]; //генерация разных значений
+                for (int i = 0; i < start.Length; i++)
+                {
+                    //checkArr[i] = rand;
+                    Icon[i].Location = start[rand];
+
+                }
+            }
+
+            public void down(PictureBox[] icon) //движение иконки вниз
+            {
+                for (int i = 0; i < icon.Length; i++)
+                {
+                    icon[i].Location = new Point(icon[i].Location.X, icon[i].Location.Y + 100);
+                }
+            }
+
+            private void UpdateTimer_Tick(object sender, EventArgs e)
+            {
+                Point start1 = new Point(100, 0);
+                Point start2 = new Point(200, 0);
+                Point[] start = new Point[] { start1, start2 }; //количество позиций
+
+                PictureBox[] Icon = new PictureBox[] { Icon1, Icon2 };
+                down(Icon);
+
+            }
+
+            private void pictureBox1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void move(object sender, PreviewKeyDownEventArgs e)
+            {
+
+                if (e.KeyData == Keys.Left)
+                    player.Location = new Point(player.Location.X - 145, player.Location.Y);
+
+                if (e.KeyData == Keys.Right)
+                    player.Location = new Point(player.Location.X + 145, player.Location.Y);
+
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
+        /*private void DrawImageRectRect(PaintEventArgs e) для перерисовки, что б не мигала картинка
+{
+
+// Create image.
+Image newImage = Image.FromFile("SampImag.jpg"); 
+
+// Create rectangle for displaying image.
+Rectangle destRect = new Rectangle(100, 100, 450, 150);
+
+// Create rectangle for source image.
+Rectangle srcRect = new Rectangle(50, 50, 150, 150);
+GraphicsUnit units = GraphicsUnit.Pixel;
+
+// Draw image to screen.
+e.Graphics.DrawImage(newImage, destRect, srcRect, units);
+}*/
+
+        //Boolean y = mainCar.Location.Y.Equals(point.Y); - сравнение когда ыконка попадет на человечка
     }
-}
+    }
+
+
+
